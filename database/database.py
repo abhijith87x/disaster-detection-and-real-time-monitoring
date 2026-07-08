af099950cd98c8fb24a95d7d5a8235396dd86e47
Abhijith B <abhijith87@gmail.com>
2026-07-08 14:46:12 +0530

    implemented retry logic database.py

--- database/database.py
+++ database/database.py
@@ -1,11 +1,17 @@
 from config import db_password
 import mysql.connector
+import time
 
-mydb = mysql.connector.connect(
-    host="mysql",
-    port=3306,
-    user="root",
-    password="root87141",
-    database="disaster_db"
-)
+while True:
+    try:
+        mydb = mysql.connector.connect(
+            host="mysql",
+            port=3306,
+            user="root",
+            password="root87141",
+            database="disaster_db"
+        )
+        break
+    except mysql.connector.Error:
+        time.sleep(2)
 cursor = mydb.cursor()
