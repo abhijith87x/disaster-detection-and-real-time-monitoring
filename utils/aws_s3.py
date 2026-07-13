4a7abe975616c1cce4273fc89c6e39c1692cccef
Abhijith B <abhijith87@gmail.com>
2026-07-13 16:02:52 +0530

    d3

--- utils/aws_s3.py
+++ utils/aws_s3.py
@@ -13,7 +13,9 @@ async def upload_file_to_s3(file):
     file.file.seek(0)
     bucket_name = AWS_BUCKET_NAME
     file_name = file.filename
-    s3.upload_fileobj(file.file, bucket_name, file_name)
+    s3.upload_fileobj(file.file, bucket_name, file_name,ExtraArgs={
+        "ContentType": file.content_type
+    })
     return f"https://{bucket_name}.s3.{AWS_REGION}.amazonaws.com/{file_name}"
 
 async def delete_file_from_s3(file_path):
