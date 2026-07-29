4603324f90a876b9e11ffe630907a9d60a62d1ec
Abhijith B <abhijith87@gmail.com>
2026-07-29 15:09:44 +0530

    aded

--- ml/disaster_model.py
+++ ml/disaster_model.py
@@ -4,18 +4,46 @@ from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-model = load_model("ml/multi-Disaster_detector3.h5", compile=False)
+
+model = None
+
+
+def get_model():
+    global model
+
+    if model is None:
+        model = load_model(
+            "ml/multi-Disaster_detector3.h5",
+            compile=False
+        )
+
+    return model
+
 
 classes = ["Earthquake","Flood","Landslide","Non_Disaster","Wildfire"]
 
+
 async def predict_disaster(file):
+
+    model = get_model()
+
     await file.seek(0)
+
     image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    image = Image.open(
+        io.BytesIO(image_bytes)
+    ).convert("RGB")
+
     image = image.resize((128,128))
+
     image_array = np.array(image)
-    image_array = image_array/255.0
+    image_array = image_array / 255.0
+
     Final_image = np.expand_dims(image_array, axis=0)
+
     prediction = model.predict(Final_image)
+
     predicted_class = classes[np.argmax(prediction)]
+
     return predicted_class
--- ml/screen_capture_model.py
+++ ml/screen_capture_model.py
@@ -2,21 +2,52 @@ import numpy as np
 import io
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+
 from ml.disaster_model import predict_disaster
 
-model = tf.keras.models.load_model("ml/screen_image_detector.h5",compile=False)
+
+model = None
+
+
+def get_screen_model():
+    global model
+
+    if model is None:
+        model = tf.keras.models.load_model(
+            "ml/screen_image_detector.h5",
+            compile=False
+        )
+
+    return model
+
 
 async def predict_screen_capture(file):
+
+    model = get_screen_model()
+
+    await file.seek(0)
+
     image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    image = image.resize((128,128))
+
+    image = Image.open(
+        io.BytesIO(image_bytes)
+    ).convert("RGB")
+
+    image = image.resize((128, 128))
+
     image_array = np.array(image)
-    image_array = image_array/255.0
-    Final_image = np.expand_dims(image_array,axis=0)
-    prediction = model.predict(Final_image)[0][0]
+    image_array = image_array / 255.0
+
+    final_image = np.expand_dims(
+        image_array,
+        axis=0
+    )
+
+    prediction = model.predict(final_image)[0][0]
+
     if prediction > 0.5:
-        return{"Screen_captured_image"}
+        return "Screen_captured_image"
+
     else:
+
         return await predict_disaster(file)
-        
