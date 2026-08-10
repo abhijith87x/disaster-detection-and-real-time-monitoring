d8776cd0a76477f2bfc6fed3e160e2226d0bee60
Abhijith B <abhijith87@gmail.com>
2026-08-11 00:30:31 +0530

    add chat bot

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -18,7 +18,7 @@ def create_test_user():
         VALUES (%s,%s,%s,%s)
         """,
         (
-            "oauth_test@gmail.com",
+            "oaut_test@gmail.com",
             "OAuth Test User",
             "google_oauth_test",
             "test.jpg"
