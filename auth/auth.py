bc82ce2c60090f65ef845b4a65db631be42e50b6
Abhijith B <abhijith87@gmail.com>
2026-08-11 21:57:37 +0530

    chat bot feature

--- auth/auth.py
+++ auth/auth.py
@@ -11,7 +11,7 @@ from jinja2 import Template
 from config import algorithm, expire_minutes
 from datetime import datetime, timedelta
 import jwt
-from jwt.jwt_handler import create_access_token, verify_token
+from ..jwt.jwt_handler import create_access_token, verify_token
 
 router = APIRouter()
 
