1faa3874cdcdfb2cf9a88cdef0a4f1cbd9732fe4
Abhijith B <abhijith87@gmail.com>
2026-09-02 23:47:12 +0530

    Fix: nginx conf

--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 from auth import router as auth_router
-
+from .config import client_secret
 
 app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key=client_secret)
 
 app.include_router(auth_router)
 
