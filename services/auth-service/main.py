a76f78e4ef609ebb494a60f1a63c6ce086094d56
Abhijith B <abhijith87@gmail.com>
2026-09-03 11:48:07 +0530

    Fix: issue router include in main

--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from auth import router as auth_router
+from Profile import router as profile_router
 from config import client_secret
 
 app = FastAPI()
 
 app.add_middleware(SessionMiddleware, secret_key=client_secret)
 
+app.include_router(profile_router)
 app.include_router(auth_router)
 
