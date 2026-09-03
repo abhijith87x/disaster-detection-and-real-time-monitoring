cd54ddaf917a96da52e260dedbb212fcf80f75ef
Abhijith B <abhijith87@gmail.com>
2026-09-03 19:57:55 +0530

    Fix: syntax cnn-service

--- services/cnn-service/predict.py
+++ services/cnn-service/predict.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from screen_capture_model import get_screen_model
 from disaster_prediction_model import predict_disaster
 
 router = APIRouter()
 
 @router.post("/detect/ScreenCapture")
-async def detect_screen_captuture(file):
+async def detect_screen_captuture(file: UploadFile = File(...)):
     result = get_screen_model(file)
     return result
 
 @router.post("/detect/disaster")
-async def detect_disaster(file):
+async def detect_disaster(file: UploadFile = File(...)):
     result = predict_disaster(file)
     return result
     
