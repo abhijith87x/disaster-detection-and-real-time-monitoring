6de0633fba9b38a6acffff1476e527a26fe8b1cc
Abhijith B <abhijith87@gmail.com>
2026-09-03 12:01:40 +0530

    Fix: file name correction

--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from auth import router as auth_router
-from Profile import router as profile_router
+from oauth_profile import router as profile_router
 from config import client_secret
 
 app = FastAPI()
