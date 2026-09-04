89c399f9fc3acc588f32cb081ceee6a4d72ea06d
Abhijith B <abhijith87@gmail.com>
2026-09-04 14:40:03 +0530

    add debug statement in grid tsx

--- frontend/src/socket_server/SocketServer.ts
+++ frontend/src/socket_server/SocketServer.ts
@@ -2,4 +2,20 @@ import { io } from "socket.io-client";
 
 const socket = io();
 
+socket.on("connect", () => {
+    console.log("🔥 CONNECTED:", socket.id);
+});
+
+socket.on("disconnect", (reason) => {
+    console.log("🔥 DISCONNECTED:", socket.id, reason);
+});
+
+socket.on("connect_error", (error) => {
+    console.log("🔥 SOCKET ERROR:", error.message);
+});
+
+socket.onAny((event, ...args) => {
+    console.log("🔥 EVENT:", event, args);
+});
+
 export default socket;
