e6536ea46604cfc0f838ea986a88f852e24495c7
Abhijith B <abhijith87@gmail.com>
2026-09-06 13:57:14 +0530

    Fix: Testing module not found  error

---
+++ tests/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("GOOGLE_CLIENT_ID")
+client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
+db_password = os.getenv("DB_PASSWORD")
+expire_minutes = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+algorithm = os.getenv("ALGORITHM")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+GOOGLE_GEMINI_KEY=os.getenv("GOOGLE_GEMINI_KEY")
