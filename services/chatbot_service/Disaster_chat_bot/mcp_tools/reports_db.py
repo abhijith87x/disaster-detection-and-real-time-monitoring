a5f43cc617bfc11fe3ad7df3206fae3d0a40551c
Abhijith B <abhijith87@gmail.com>
2026-09-08 15:18:04 +0530

    chatbot debugging

--- services/chatbot_service/Disaster_chat_bot/mcp_tools/reports_db.py
+++ services/chatbot_service/Disaster_chat_bot/mcp_tools/reports_db.py
@@ -10,7 +10,7 @@ def get_db():
                 host="mysql",
                 port=3306,
                 user="root",
-                password=db_password,
+                password="root87141",
                 database="disaster_db"
             )
         except mysql.connector.Error:
