4c45739e8516686c4e1952886fa7e728af5f7806
Abhijith B <abhijith87@gmail.com>
2026-09-08 15:13:46 +0530

    chatbot debugging

--- services/disaster_service/database.py
+++ services/disaster_service/database.py
@@ -1,17 +1,31 @@
-from config import db_password
-
+from ..config import db_password
 import mysql.connector
 import time
+import sys
 
 def get_db():
+
     while True:
         try:
-            return mysql.connector.connect(
+            print("Trying MySQL connection...", file=sys.stderr, flush=True)
+
+            connection = mysql.connector.connect(
                 host="mysql",
                 port=3306,
                 user="root",
                 password=db_password,
                 database="disaster_db"
             )
-        except mysql.connector.Error:
+
+            print("MySQL connection successful!", file=sys.stderr, flush=True)
+
+            return connection
+
+        except mysql.connector.Error as e:
+            print(
+                f"MySQL connection failed: {e}",
+                file=sys.stderr,
+                flush=True
+            )
+
             time.sleep(2)
