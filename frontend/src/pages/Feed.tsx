c546a4b5c8eda3d8d91de449c9afa7bb98087f94
Abhijith B <abhijith87@gmail.com>
2026-09-19 15:52:02 +0530

    add welcome notification

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -7,16 +7,16 @@ interface TileStyle extends React.CSSProperties {
 
 function Feed() {
     useEffect(() => {
-    // const hasSeenWelcome = localStorage.getItem("welcomeShown");
+    const hasSeenWelcome = sessionStorage.getItem("welcomeShown");
 
-    // if (!hasSeenWelcome) {
+    if (!hasSeenWelcome) {
         alert(
-           "Welcome to the Disaster Management System! Use the camera icon at the bottom right to upload real disaster images. Use the Demo button at the top right for testing and demonstration purposes, since real disaster incidents cannot be created for testing."
+           "Welcome to the Disaster Management System! Use the camera icon at the bottom right to upload real disaster images. Use the Demo button at the top right for testing and demonstration purposes, since real disaster incidents cannot be created for testing.\n\nLike, dislike, and report actions have a threshold of 2 users each."
         );
 
-        // localStorage.setItem("welcomeShown", "true");
-    // }
-    }, [0]);
+        sessionStorage.setItem("welcomeShown", "true");
+    }
+    }, []);
     return (
         <div className="main-body">
             <div className="image">
